4b33297c7705f37c7766f9068483effaa8a3870d
alexbzmn <crashius@gmail.com>
2017-07-12 23:03:16 +0200

    fibonacci dynamic programming version

--- src/com/alexbzmn/Fibonacci.java
+++ src/com/alexbzmn/Fibonacci.java
@@ -18,11 +18,25 @@ public class Fibonacci {
         return fibonacci(n - 2) + fibonacci(n - 1);
     }
 
+    public static int fibonacciD(int n) {
+        int[] memo = new int[n];
+        memo[0] = 1;
+        memo[1] = 1;
+        memo[2] = 2;
+
+        for (int i = 3; i < memo.length; i++) {
+            memo[i] += memo[i-1] + memo[i-2];
+        }
+
+        return memo[n - 1];
+    }
+
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.close();
-        System.out.println(fibonacci(n));
+        System.out.println(fibonacciD(n));
     }
 }
