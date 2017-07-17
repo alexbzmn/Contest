125dffb30c103962fbbec4c2e2ccb57f4d55f7a5
alexbzmn <crashius@gmail.com>
2017-07-17 20:14:21 +0200

    once in a tram brute-force solution

--- src/com/alexbzmn/OnceInATram.java
+++ src/com/alexbzmn/OnceInATram.java
@@ -7,7 +7,20 @@ import java.util.Scanner;
  * @since 17.07.2017
  */
 public class OnceInATram {
-    static String onceInATram(int x) {
+    static private String onceInATram(int x) {
+        while (true) {
+            int[] a = getArray(++x);
+
+            int first = a[0] + a[1] + a[2];
+            int second = a[3] + a[4] + a[5];
+
+            if (first == second) {
+                return String.valueOf(x);
+            }
+        }
+    }
+
+    private static int[] getArray(int x) {
         String s = String.valueOf(x);
         String[] arr = s.split("");
 
@@ -16,103 +29,7 @@ public class OnceInATram {
             a[i] = Integer.valueOf(arr[i]);
         }
 
-        int sumOne = a[0] + a[1] + a[2];
-        int sumTwo = a[3] + a[4] + a[5];
-
-        if (sumOne == sumTwo) {
-            int deltaI = a.length - 1;
-
-            for (int i = a.length - 1; i >= 3; i--) {
-                if (a[i] > 0) {
-                    a[i] = a[i]--;
-                    deltaI = i;
-                    break;
-                }
-            }
-
-            for (int i = a.length - 1; i >= 3; i--) {
-                if (i != deltaI) {
-                    if (a[i] < 9) {
-                        a[i] = a[i]++;
-                        break;
-                    }
-                }
-            }
-
-        } else if (sumOne > sumTwo) {
-            int diff = sumOne - sumTwo;
-            while (sumOne > sumTwo) {
-
-                while (a[5] < 9) {
-                    if (diff > 0) {
-                        a[5] = a[5] + 1;
-                        diff--;
-                    } else {
-                        break;
-                    }
-                }
-
-                while (a[4] < 9) {
-                    if (diff > 0) {
-                        a[4] = a[4] + 1;
-                        diff--;
-                    } else {
-                        break;
-                    }
-                }
-
-                while (a[3] < 9) {
-                    if (diff > 0) {
-                        a[3] = a[3] + 1;
-                        diff--;
-                    } else {
-                        break;
-                    }
-                }
-
-                sumTwo = a[3] + a[4] + a[5];
-            }
-        } else if (sumTwo > sumOne) {
-            int diff = sumTwo - sumOne;
-            while (sumTwo > sumOne) {
-
-                while (a[2] < 9) {
-                    if (diff > 0) {
-                        a[2] = a[2] + 1;
-                        diff--;
-                    } else {
-                        break;
-                    }
-                }
-
-                while (a[1] < 9) {
-                    if (diff > 0) {
-                        a[1] = a[1] + 1;
-                        diff--;
-                    } else {
-                        break;
-                    }
-                }
-
-                while (a[0] < 9) {
-                    if (diff > 0) {
-                        a[0] = a[0] + 1;
-                        diff--;
-                    } else {
-                        break;
-                    }
-                }
-
-                sumOne = a[0] + a[1] + a[2];
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int el : a) {
-            sb.append(el);
-        }
-
-        return sb.toString();
+        return a;
     }
 
     public static void main(String[] args) {
