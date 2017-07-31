2d8a3a3952f71cfbe0b94040fa217bc77c4f9e7a
alexbzmn <crashius@gmail.com>
2017-07-31 22:20:32 +0200

    insertion sort added

--- src/com/alexbzmn/MergeSort.java
+++ src/com/alexbzmn/MergeSort.java
@@ -1,59 +1,75 @@
 package com.alexbzmn;
 
-import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
-import java.util.Scanner;
 
 public class MergeSort {
 
     private static long inversionCount = 0;
 
-//    public static void main(String[] args) {
-////        int[] a = {6, 4, 2, 7, 3, -1, 0, 2}; //-1 0 2 2 3 4 6 7
-//        long s = System.currentTimeMillis();
-//
-//        int[] a = new Random().ints(1000000, -1000, 1000).toArray();
-////        System.out.println(Arrays.toString(a));
-//
-//        sort(a);
-//
-////        Arrays.sort(a);
-//
-//        for (int i = 1; i < a.length; i++) {
-//
-//            if (a[i] < a[i - 1]) {
-//                System.out.println("Assertion failed");
-//            }
-//        }
-//
-////        System.out.println(Arrays.toString(a));
-//
-//        long f = System.currentTimeMillis();
-//
-//        System.out.println("TIME: " + (f - s));
-//    }
-
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        int[] a = {6, 4, 2, 7, 3, -1, 0, 2}; //-1 0 2 2 3 4 6 7
         long s = System.currentTimeMillis();
 
-        Scanner in = new Scanner(new File("files/MSINVIN.txt"));
-        int t = in.nextInt();
-        for (int a0 = 0; a0 < t; a0++) {
-            int n = in.nextInt();
-            int arr[] = new int[n];
-            for (int arr_i = 0; arr_i < n; arr_i++) {
-                arr[arr_i] = in.nextInt();
+//        int[] a = new Random().ints(1000000, -1000, 1000).toArray();
+//        System.out.println(Arrays.toString(a));
+
+//        sort(a);
+        insertionSort(a);
+
+//        Arrays.sort(a);
+
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i] < a[i - 1]) {
+                System.out.println("Assertion failed");
             }
-            System.out.println(countInversions(arr));
         }
+
+        System.out.println(Arrays.toString(a));
 
         long f = System.currentTimeMillis();
 
-        System.out.println(f - s);
+        System.out.println("TIME: " + (f - s));
     }
+
+
+    private static void insertionSort(int[] a) {
+
+        for (int i = 1; i < a.length; i++) {
+            int j = i;
+
+            while (j > 0 && a[j] < a[j - 1]) {
+                int b = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = b;
+
+                j--;
+            }
+        }
+
+    }
+
+
+//    public static void main(String[] args) throws Exception {
+//        long s = System.currentTimeMillis();
+//
+//        Scanner in = new Scanner(new File("files/MSINVIN.txt"));
+//        int t = in.nextInt();
+//        for (int a0 = 0; a0 < t; a0++) {
+//            int n = in.nextInt();
+//            int arr[] = new int[n];
+//            for (int arr_i = 0; arr_i < n; arr_i++) {
+//                arr[arr_i] = in.nextInt();
+//            }
+//            System.out.println(countInversions(arr));
+//        }
+//
+//        long f = System.currentTimeMillis();
+//
+//        System.out.println(f - s);
+//    }
 
     public static long countInversions(int[] arr) {
         inversionCount = 0;
