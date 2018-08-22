c0508de2e214f8695c7c248c5d257d8839ed53cf
alexbzmn <crashius@gmail.com>
2018-08-22 14:31:32 +0200

    add right rotation impl

--- src/main/scala/com/alexbzmn/contest/arrays/LeftRotation.scala
+++ src/main/scala/com/alexbzmn/contest/arrays/LeftRotation.scala
@@ -4,11 +4,19 @@ object LeftRotation {
 
   def main(args: Array[String]): Unit = {
     val arr = Array(1, 2, 3, 4, 5)
+    val shift = 4
+    val indices = arr.indices
+      .map(i => (arr.length - 1) - i)
+      .map(i => {
+        if (i + shift > arr.length - 1) (i + shift) % arr.length
+        else i + shift
+      })
+    //      .map(i => (i + 4) % arr.length)
+    //      .map(i => arr.length - i)
 
-    arr.indices
-      .map(i => arr.length - i)
-      .map(i => arr.length % arr.length + 4)
+    val newArr: Array[Int] = new Array[Int](arr.length)
+    (arr.reverse, indices).zipped.foreach((x, idx) => newArr(idx) = x)
 
-    println()
+    println(newArr.map(_.toString) reduce ((x, y) => x + " " + y))
   }
 }
