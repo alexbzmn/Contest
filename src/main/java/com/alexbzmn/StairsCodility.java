45b20b7ed28ebd3ae41e9e5e78c44f32d62e474e
Aleksei Batcman <crashius@gmail.com>
2018-10-07 12:30:05 +0200

    StairsCodility

--- src/main/java/com/alexbzmn/StairsCodility.java
+++ src/main/java/com/alexbzmn/StairsCodility.java
@@ -1,6 +1,19 @@
 package com.alexbzmn;
 
+import java.util.Arrays;
+
 public class StairsCodility {
+
+	public static void main(String[] args) {
+		//		System.out.println(calculateWays(2));
+		//		System.out.println(calculateWays(3));
+		//		System.out.println(calculateWays(4));
+		//		System.out.println(calculateWays(5));
+		//		System.out.println(calculateWays(6));
+		//		System.out.println(Arrays.toString(genericStairsProblem(50000, 2)));
+		System.out.println(Arrays.toString(solutionMem(new int[] { 4, 4, 5, 5, 1 }, new int[] { 3, 2, 4, 3, 1 })));
+		System.out.println(Arrays.toString(solutionMem(new int[] { 50000 }, new int[] { 30})));
+	}
 
 	public int[] solution(int[] A, int[] B) {
 		int[] result = new int[A.length];
@@ -12,7 +25,7 @@ public class StairsCodility {
 		return result;
 	}
 
-	private int calculateWays(int n) {
+	private static int calculateWays(int n) {
 
 		if (n == 0) {
 			return 1;
@@ -32,5 +45,71 @@ public class StairsCodility {
 
 		return 0;
 	}
+
+	public static int[] solutionMem(int[] A, int[] B) {
+
+		int max = 0;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] > max) {
+				max = A[i];
+			}
+		}
+
+		int maxLimit = 0;
+		for (int i = 0; i < B.length; i++) {
+			if (B[i] > maxLimit) {
+				maxLimit = B[i];
+			}
+		}
+
+		maxLimit = (1 << maxLimit) - 1;
+
+		int[] mem = createReducedFibArray(max + 1, maxLimit);
+
+		int[] result = new int[A.length];
+		for (int i = 0; i < A.length; i++) {
+			result[i] = mem[A[i]] & ((1 <<B[i]) - 1);
+		}
+
+		return result;
+	}
+
+	public static int[] createReducedFibArray(int n, int limit) {
+		int[] mem = new int[n];
+		mem[0] = 1;
+
+		if (n == 1) {
+			return mem;
+		}
+
+		mem[1] = 1;
+
+		for (int i = 2; i < n; i++) {
+			mem[i] = mem[i - 1] + mem[i - 2] & limit;
+		}
+
+		return mem;
+	}
+	//
+	//	public static long[] genericStairsProblem(int n, int m) {
+	//		long[] mem = new long[n];
+	//		mem[0] = 1;
+	//
+	//		if (n == 1) {
+	//			return mem;
+	//		}
+	//
+	//		mem[1] = 1;
+	//
+	//		for (int i = 2; i < n; i++) {
+	//			mem[i] = 0;
+	//
+	//			for (int j = 1; j <= m && j <= i; j++) {
+	//				mem[i] += mem[i - j];
+	//			}
+	//		}
+	//
+	//		return mem;
+	//	}
 
 }
