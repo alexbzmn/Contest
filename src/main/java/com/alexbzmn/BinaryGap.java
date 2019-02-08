5d86e034cdfebf24224af561c957d77e0d2fd69d
Aleksei Batcman <crashius@gmail.com>
2019-02-08 13:45:25 +0300

    BinaryGap

--- src/main/java/com/alexbzmn/BinaryGap.java
+++ src/main/java/com/alexbzmn/BinaryGap.java
@@ -2,4 +2,27 @@ package com.alexbzmn;
 
 public class BinaryGap {
 
+	public int solution(int N) {
+		String bin = Integer.toBinaryString(N);
+
+		int longest = 0;
+		char prev = ' ';
+		int len = 0;
+
+		for (char c : bin.toCharArray()) {
+			if (c == '1') {
+				if (prev == '0') {
+					longest = Math.max(longest, len);
+					len = 0;
+				}
+
+			} else {
+				len++;
+			}
+
+			prev = c;
+		}
+
+		return longest;
+	}
 }
--- src/main/java/com/alexbzmn/FrogRiverOne.java
+++ src/main/java/com/alexbzmn/FrogRiverOne.java
@@ -1,3 +1,5 @@
+package com.alexbzmn;
+
 import java.util.HashSet;
 import java.util.Set;
 
