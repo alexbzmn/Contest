f3fa78d4e7c3462b4ec034e9d5a3e555d15e2bfb
Aleksei Batcman <crashius@gmail.com>
2019-02-09 18:07:18 +0300

    enhance TapeEquilibrium solution

--- src/main/java/com/alexbzmn/TapeEquilibrium.java
+++ src/main/java/com/alexbzmn/TapeEquilibrium.java
@@ -2,8 +2,12 @@ package com.alexbzmn;
 
 public class TapeEquilibrium {
 
-	public int solution(int[] A) {
+	public static int solution(int[] A) {
 		int sum = 0;
+
+		if (A.length == 2) {
+			return Math.abs(A[0] - A[1]);
+		}
 
 		for (int i = 0; i < A.length; i++) {
 			sum += A[i];
@@ -13,11 +17,19 @@ public class TapeEquilibrium {
 		int cumSum = 0;
 
 		for (int i = 0; i < A.length; i++) {
-			cumSum += A[i];
-			min = Math.min(min, Math.abs(A[i] - (sum - cumSum)));
+			if (i == A.length - 1) {
+				min = Math.min(min, Math.abs(A[i] - cumSum));
+			} else {
+				cumSum += A[i];
+				min = Math.min(min, Math.abs(cumSum - (sum - cumSum)));
+			}
 		}
 
 		return min;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution(new int[] { -10, -20, -30, -40, 100 }));
 	}
 
 }
