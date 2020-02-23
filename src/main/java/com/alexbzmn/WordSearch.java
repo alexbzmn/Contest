930682b1c776ff098fc0548a52a9fa7d1248d5ae
Aleksei Batcman <crashius@gmail.com>
2020-02-23 11:06:14 +0000

    use array to store seen instead

--- src/main/java/com/alexbzmn/WordSearch.java
+++ src/main/java/com/alexbzmn/WordSearch.java
@@ -6,20 +6,36 @@ import java.util.List;
 import java.util.Set;
 
 public class WordSearch {
+    public static void main(String[] args) {
+        int n = 30;
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'a';
+            }
+        }
+
+        board[n - 1][n - 1] = 'b';
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('b');
+        for (int i = 0; i < (n * n) - 1; i++) {
+            sb.append('a');
+        }
+        System.out.println(new WordSearch().exist(board, sb.toString()));
+    }
+
     public boolean exist(char[][] board, String word) {
         if (word.length() == 0 || (board.length * board[0].length) < word.length()) {
             return false;
         }
 
-        List<Integer> v = new ArrayList<>();
-        List<Integer> h = new ArrayList<>();
-
+        Set<String> first = new HashSet<>();
         char f = word.charAt(0);
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == f) {
-                    v.add(i);
-                    h.add(j);
+                    first.add(i + "" + j);
                 }
             }
         }
@@ -28,10 +44,9 @@ public class WordSearch {
         for (char c : word.toCharArray()) {
             w.add(c);
         }
-
-        for (int i = 0; i < v.size(); i++) {
-            Set<String> seen = new HashSet<>();
-            boolean res = find(v.get(i), h.get(i), board, w, 0, seen);
+        boolean[][] seen = new boolean[board.length][board[0].length];
+        for (String pair : first) {
+            boolean res = find(Integer.parseInt(pair.substring(0, pair.length() / 2)), Integer.parseInt(pair.substring(pair.length() / 2)), board, w, 0, seen);
             if (res) {
                 return true;
             }
@@ -40,7 +55,7 @@ public class WordSearch {
         return false;
     }
 
-    private boolean find(int i, int j, char[][] b, List<Character> w, int l, Set<String> seen) {
+    private boolean find(int i, int j, char[][] b, List<Character> w, int l, boolean[][] seen) {
         if (l > w.size() - 1) {
             return true;
         }
@@ -49,21 +64,19 @@ public class WordSearch {
             return false;
         }
 
-        String key = i + "" + j;
-        if (!seen.contains(key) && b[i][j] == w.get(l)) {
-            seen.add(key);
+        if (!seen[i][j] && b[i][j] == w.get(l)) {
+            seen[i][j] = true;
             boolean result =
                     find(i + 1, j, b, w, l + 1, seen) ||
                             find(i - 1, j, b, w, l + 1, seen) ||
                             find(i, j + 1, b, w, l + 1, seen) ||
                             find(i, j - 1, b, w, l + 1, seen);
             if (!result) {
-                seen.remove(key);
+                seen[i][j] = false;
             }
             return result;
         }
 
         return false;
     }
-
 }
